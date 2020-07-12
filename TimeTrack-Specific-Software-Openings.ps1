a359b93d49e2d97bac381fac186ed3940bd70d26
Henrik Johnsen <29645801+Am0rphous@users.noreply.github.com>
2020-07-12 15:43:46 +0200

    Update TimeTrack-Specific-Software-Openings.ps1

--- TimeTrack-Specific-Software-Openings.ps1
+++ TimeTrack-Specific-Software-Openings.ps1
@@ -1,16 +1,16 @@
-<#Primitive script used to test how long time various programs needs to open.
-VLC and Windows Media Player is used for simplicity.
+<#Primitive script used to test how long time a specific program need to be opened.
+VLC and Windows Media Player is used as examples, but any program may be used.
 Modern servers will have a close average time, while old and unstable servers will vary more.#>
 
 #Testing VLC or Windows Media Player?
 [string] $ProgramNavn = "vlc"
 #[string] $ProgramNavn = "wmplayer"
 
-# Windows 7
+# Windows 7 paths:
 Set-Location 'C:\Program Files\VideoLAN\VLC\'
 #Set-Location 'C:\Program Files\Windows Media Player\'
 
-#Windows 10:
+#Windows 10 paths:
 #Set-Location 'C:\Program Files (x86)\VideoLAN\VLC\'
 #Set-Location 'C:\Program Files\Windows Media Player\'
 
@@ -23,6 +23,7 @@ while ( $teller -lt 1001) { #Velger 101 stk for å få 100 i tabellen.
 
 #Husk å endre under:
     [int]$tid = "0"
+    
     $tid = Measure-Command { .\vlc.exe } | Select -ExpandProperty Milliseconds
     #$tid = Measure-Command { .\wmplayer.exe } | Select -ExpandProperty Milliseconds
 
@@ -38,7 +39,7 @@ while ( $teller -lt 1001) { #Velger 101 stk for å få 100 i tabellen.
             Write-Host " Kjøring nr. $teller viser $tid <-" -Fore Red
         }
 
-    #Legge sammen all tiden.
+    #Legger sammen all tiden.
 
     $SammenLagtTid = $SammenLagtTid + $tid
 
